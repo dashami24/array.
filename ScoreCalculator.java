7365d4f9d20fdd7a9026b46b630c1764f0805645
dashami24 <dashamim24@email.com>
2025-06-11 10:18:49 +0530

    Add max and min score calculation to ScoreCalculator

--- ScoreCalculator.java
+++ ScoreCalculator.java
@@ -3,17 +3,26 @@ public class ScoreCalculator {
         int[] scores = {85, 90, 78, 92, 88};
 
         int sum = 0;
+        int max = scores[0];
+        int min = scores[0];
+
         for (int score : scores) {
             sum += score;
-            int max = scores[0];
-            int min = scores[0];
 
+            if (score > max) {
+                max = score;
+            }
+
+            if (score < min) {
+                min = score;
+            }
         }
 
         double average = (double) sum / scores.length;
 
         System.out.println("Sum: " + sum);
         System.out.println("Average: " + average);
-
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
     }
 }
